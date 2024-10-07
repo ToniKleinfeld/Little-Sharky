448b5120cf2f7458338d9c332b831093fa00c71b
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2024-10-07 15:07:34 +0200

    add resposive and spawn animation boss

--- models/drawable-objects.class.js
+++ models/drawable-objects.class.js
@@ -21,7 +21,13 @@ class DrawableObject {
     }
 
     draw(ctx) {
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+        
+        try {
+            ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+        } catch (e) {
+            console.warn('Error loading image', e);
+            console.log('Could not load Image', this.img.src)
+        }
     }  
 
     drawFrame(ctx) {
