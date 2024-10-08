8336e0079d18a7b70309ad2e20227369e701321b
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2024-10-08 15:15:35 +0200

    add mobile buttons layout

--- js/game.js
+++ js/game.js
@@ -39,4 +39,34 @@ document.addEventListener('keyup', (e) => {
     }
 })
 
+document.addEventListener('touchstart', (e) => {   
+    if (e.target.id == 'rightButton') {
+        keyboard.right = true;        
+    } else if (e.target.id == 'topButton') {
+        keyboard.top = true;
+    } else if (e.target.id == 'leftButton') {
+        keyboard.left = true;
+    } else if (e.target.id == 'downButton') {
+        keyboard.down = true;
+    } else if (e.target.id == 'spaceButton') {
+        keyboard.space = true;
+    } else if (e.target.id == 'dButton') {
+        keyboard.d = true;
+    } 
+})
+
+document.addEventListener('touchend', (e) => {  
+    if (e.target.id == 'rightButton') {
+        keyboard.right = false;        
+    } else if (e.target.id == 'topButton') {
+        keyboard.top = false;
+    } else if (e.target.id == 'leftButton') {
+        keyboard.left = false;
+    } else if (e.target.id == 'downButton') {
+        keyboard.down = false;
+    }  else if (e.target.id == 'dButton') {
+        keyboard.d = false;
+    } 
+})
+
 InitLevel() // load level 1 <-- use later for start button when overlay designed
